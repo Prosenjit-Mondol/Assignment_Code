bbcfdbad585009b675651285f6144f1aa89a9376
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-03 10:06:17 +0600

    Add Code

---
+++ Ninteen.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab4;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.*;
+public class Ninteen {
+    public static void main(String[] args)
+    {
+        String str="Python";
+        System.out.println(str.substring(0,str.length()/2));
+    }
+}
